24a053a5e614aaccbff17bdac74bc59ba9308964
nilesh1811 <nileshrpatil1811@gmail.com>
2020-12-23 13:27:18 +0530

    design

--- application/controllers/WebsiteController.php
+++ application/controllers/WebsiteController.php
@@ -51,10 +51,10 @@ class WebsiteController extends CI_Controller{
    /**************************      Home Page      ********************************/
   public function index(){
 
-    $quizweb_user_id = $this->session->userdata('quizweb_user_id');
-    $quizweb_company_id = $this->session->userdata('quizweb_company_id');
-    $quizweb_roll_id = $this->session->userdata('quizweb_roll_id');
-    if($quizweb_user_id == '' && $quizweb_company_id == '' && $quizweb_roll_id ==''){ header('location:'.base_url().'WebsiteController'); }
+    // $quizweb_user_id = $this->session->userdata('quizweb_user_id');
+    // $quizweb_company_id = $this->session->userdata('quizweb_company_id');
+    // $quizweb_roll_id = $this->session->userdata('quizweb_roll_id');
+    // if($quizweb_user_id == '' && $quizweb_company_id == '' && $quizweb_roll_id ==''){ header('location:'.base_url().'WebsiteController'); }
     $data['banner_list'] = $this->Website_Model->banner_list('bannerid');
     $data['tab_list'] = $this->Website_Model->tab_list('tabinputtextid');
     $data['competition_list'] = $this->Website_Model->competition_list('competitionid','','','','','','competition');
@@ -285,11 +285,11 @@ class WebsiteController extends CI_Controller{
   }
 
 
-     public function about()
-    {
-      $this->load->view('Website/Include/head');
-    $this->load->view('Website/index');
-    $this->load->view('Website/Include/footer');
-    }
+    //  public function about()
+    // {
+    //   $this->load->view('Website/Include/head');
+    // $this->load->view('Website/index');
+    // $this->load->view('Website/Include/footer');
+    // }
 
 }
